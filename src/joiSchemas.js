a9d4b4bc90b0bfa12ee03ed96f3b296258d02ee6
Yohan Lopes <yohanhxh@gmail.com>
2021-10-16 15:42:03 -0300

    adding route /games

--- src/joiSchemas.js
+++ src/joiSchemas.js
@@ -3,3 +3,11 @@ import joi from 'joi';
 export const categorieSchema = joi.object({
     name: joi.string().required()
 })
+
+export const gameSchema = joi.object({
+    name: joi.string().required(),
+    image: joi.string(),
+    stockTotal: joi.number().min(1).required(),
+    categoryId: joi.number().required(),
+    pricePerDay: joi.number().min(1).required()
+})
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import pg from 'pg';
 import dotenv from 'dotenv';
-import { categorieSchema } from './joiSchemas.js'
+import {
+    categorieSchema,
+    gameSchema
+} from './joiSchemas.js'
 
 dotenv.config()
 const { Pool } = pg;
@@ -17,39 +20,112 @@ const connection = new Pool({
 
 routes.get("/categories", async (req, res) => {
     try {
-        const result = await connection.query('SELECT * FROM categories')
-        res.json(result.rows) //when i want to send an array, should the res.json be used?
+        const dbResponse = await connection.query('SELECT * FROM categories;')
+        return res.json(dbResponse.rows) //when i want to send an array, should the res.json be used?
     } catch(e) {
         console.log("ERRO GET /categories");
         console.log(e)
-        res.sendStatus(500)
+        return res.sendStatus(500)
     }
 })
 
 routes.post("/categories", async (req, res) => {
     try {
         const isValid = !categorieSchema.validate(req.body).error
-        const exists = await connection.query(`SELECT * FROM categories WHERE name = $1`,
-            [req.body.name.toLowerCase()])
+        if(!isValid) return res.sendStatus(422)
 
-        if(exists.rows.length > 0) {
-            res.sendStatus(409)
-            return;
-        }
+        const exists = await connection.query('SELECT * FROM categories WHERE name iLIKE $1;',
+            [req.body.name])
+        if(exists.rows.length > 0) return res.sendStatus(409)
 
-        if(isValid) {
-            await connection.query(`INSERT INTO categories (name) VALUES ($1)`,
-                [req.body.name.toLowerCase()])
+        await connection.query('INSERT INTO categories (name) VALUES ($1);',
+            [req.body.name])
 
-            res.sendStatus(201)
-            return;
-        }
-        res.sendStatus(422)
+        return res.sendStatus(201)
     } catch (e) {
         console.log("ERRO POST /categories")
+        console.log(e)
+        return res.sendStatus(500)
+    }
+})
+
+routes.get("/games", async (req, res) => {
+    try {
+        const { name } = req.query
+        if(name) {
+            const dbResponse = await connection.query(
+                'SELECT * FROM games WHERE name iLIKE $1',
+                [name+"%"]
+            )
+            return res.json(dbResponse.rows)
+        }
+
+        const dbResponse = await connection.query('SELECT * FROM games')
+        return res.json(dbResponse.rows)
+    } catch(e) {
+        console.log("ERRO GET /games")
+        console.log(e)
+        return res.sendStatus(500)
+    }
+})
+
+routes.post("/games", async (req, res) => {
+    try {
+        const isValid = !gameSchema.validate(req.body).error
+        if(!isValid) return res.sendStatus(400)
+
+        const {
+            name,
+            image,
+            stockTotal,
+            categoryId,
+            pricePerDay
+        } = req.body
+
+        const categoriesFound = connection.query('SELECT * FROM categories WHERE id = $1;',
+            [req.body.categoryId])
+        const nameFound = connection.query('SELECT * FROM games WHERE name = $1;',
+            [req.body.name])
+
+        const dbConnections = await Promise.all([ categoriesFound, nameFound ])
+
+        if(dbConnections[0].rows.length === 0) return res.sendStatus(400)
+        if(dbConnections[1].rows.length > 0) return res.sendStatus(409)
+
+        await connection.query(
+            `INSERT INTO games 
+                (name, image, "stockTotal", "categoryId", "pricePerDay")
+             VALUES
+                ($1, $2, $3, $4, $5);`,
+            [name, image, stockTotal, categoryId, pricePerDay]
+        )
+
+        return res.sendStatus(201)
+
+    } catch(e) {
+        console.log("ERRO POST /games")
         console.log(e)
         res.sendStatus(500)
     }
 })
 
+routes.get("/customers", async (req, res) => {
+    try {
+        const { cpf } = req.query
+        if(cpf) {
+            const dbResponse = await connection.query(
+                'SELECT * FROM customers WHERE cpf iLIKE $1',
+                [cpf+"%"]
+            )
+            return res.send(dbResponse.rows)
+        }
+
+        const dbResponse = await connection.query('SELECT * FROM customers')
+        return res.send(dbResponse.rows)
+    } catch(e) {
+        console.log("ERRO GET /customers");
+        console.log(e)
+        return res.sendStatus(500)
+    }
+})
 export default routes;
